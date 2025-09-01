b4c4ae03ded8b50595ccd3369e6ec5b3eb1beb32
Ravi Kishan <kishanravi887321@gmail.com>
2025-09-01 22:34:55 +0530

    Add doctor route to accounts views for API health check

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from ninja import NinjaAPI
+from django.contrib.auth import authenticate
 
+router=NinjaAPI()
+
+
+# // making the doctor route for check 
+
+router.get("/doctor")
+def doctor(req):
+    return {"message":"The API is working fine"}
 # Create your views here.
